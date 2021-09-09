668eb1abac10298bdfc04672cd695be189e20eaf
Rakibul santo <SANTODON176@gmail.com>
2021-09-09 20:27:00 +0600

    add

--- scripts.js
+++ scripts.js
@@ -1,24 +1,20 @@
-// function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(String(email).toLowerCase());
-// }
-
+const error = document.getElementById('error')
 const validateEmail =()=>{
 
 const email = document.getElementById('input-field')
 const writeEmail = email.value;
-
+email.value = '';
 const cheakEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const cheak = (cheakEmail.test(writeEmail));
 
 if(cheak === true){
-  email.style.background = 'rgba(34, 226, 44, 0.993)';
-  email.style.color = 'black';
+  error.innerHTML = `<P style = "background:#479F76";  class= " p-3 ">Login Successfully </p>`
 }
 else{
-  email.style.background = 'rgba(221, 60, 60, 0.986)';
-  email.style.color = 'white';
+
+  error.innerHTML = `<P style = "background:#EA868F";  class= " p-3 "> Please enter a valid email </p>`
+ 
 }
 }
 
